3da6a5d614cd22273816b42c4a038d90a1bd8d02
Nick Lehmann <nick@lehmann.sh>
2022-03-20 16:00:52 +0100

    Add database setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,29 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
 extern crate tokio;
 use std::net::TcpListener;
 
-use actix_web::{dev::Server, get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{dev::Server, get, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
 
 #[get("/health")]
 async fn health() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
+#[derive(Deserialize)]
+struct SubscriptionData {
+    pub name: String,
+    pub email: String,
+}
+
+#[post("/subscriptions")]
+async fn subscribe(form: web::Form<SubscriptionData>) -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> std::io::Result<Server> {
-    let server = HttpServer::new(|| App::new().service(health))
+    let server = HttpServer::new(|| App::new().service(health).service(subscribe))
         .listen(listener)?
         .run();
 
